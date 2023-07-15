727702c0fcae172ddceb91a1ea77c93c3b81b06f
AmnaZa <113046745+AmnaZa@users.noreply.github.com>
2023-07-15 01:00:20 -0400

    code Updated

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Header from './components/Header';
+import BlogPost from './components/BlogPost';
+import Footer from './components/Footer';
+import './App.css';
+import image1 from './images/image1.jpg';
+import image2 from './images/image2.jpg';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <BlogPost
+          title="On the Street in Brookly"
+          content="Cray ipsum, dolor sit amet consectetur adipisicing elit. Repellat quidem deserunt
+          nemo dignissimos fuga veniam inventore eligendi magnam aperiam odit mollitia
+          cupiditate eveniet velit officiis magni aliquid, laborum dolor? Quasi? Veniam
+          amet rerum ducimus est ea at neque alias. Temporibus perspiciatis at impedit
+          voluptas aut voluptates facere totam aliquid debitis rerum, ex consequuntur, nulla eius quae
+          corrupti optio atque ut. Repellendus molestiae doloribus recusandae, itaque voluptatibus
+          placeat repellat deserunt amet eaque dignissimos, iste sapiente magnam doloremque. Fugit,
+          cum, cupiditate aliquam, mollitia quisquam sed nulla eveniet doloribus neque optio odio qui!
+          Id architecto impedit consequatur rem quaerat voluptatibus pariatur quas, quod quisquam
+          quo ab molestiae.."
+          imageUrl={image1}
+        />
+        <BlogPost
+          title="Vintage in Vogue"
+          content="Selfies sunt, dolor sit amet consectetur adipisicing elit. Repellat quidem deserunt
+          nemo dignissimos fuga veniam inventore eligendi magnam aperiam odit mollitia
+          cupiditate eveniet velit officiis magni aliquid, laborum dolor? Quasi? Veniam amet
+          rerum ducimus est ea at neque alias. Temporibus perspiciatis at impedit voluptas
+          aut voluptates facere totam aliquid debitis rerum, ex consequuntur, nulla eius quae corrupti
+          optio atque ut. Repellendus molestiae doloribus recusandae, itaque voluptatibus placeat
+          repellat deserunt amet eaque dignissimos, iste sapiente magnam doloremque. Fugit, cum,
+          cupiditate aliquam, mollitia quisquam sed nulla eveniet doloribus neque optio odio qui! Id
+          architecto impedit consequatur."
+          imageUrl={image2}
+        />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/BlogPost.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function BlogPost({ title, content, imageUrl }) {
+  return (
+    <article>
+      <h3>{title}</h3>
+      <div className="content">
+        <img src={imageUrl} alt="Blog Post" />
+        <p>{content}</p>
+      </div>
+      <a href="#" className="read-more">Continue...</a>
+    </article>
+  );
+}
+
+export default BlogPost;
