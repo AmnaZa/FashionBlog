50b7d0609fb00e5b88013df0e7b18bda0a05793d
AmnaZa <113046745+AmnaZa@users.noreply.github.com>
2023-07-16 13:27:03 -0400

    added current date to BlogPost

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,12 +1,15 @@
 
 
-function BlogPost({ title, content, imageUrl }) {
+function BlogPost({ title, imageUrl, children }) {
+  const currentDate = new Date().toLocaleDateString();
+
   return (
     <article>
       <h3>{title}</h3>
+      <p>{currentDate}</p>
       <div className="content">
         <img src={imageUrl} alt="Blog Post" />
-        <p>{content}</p>
+        {children}
       </div>
       <a href="#" className="read-more">Continue...</a>
     </article>
